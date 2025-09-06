d100573a007958671bb581d26a25cbfbe04f6d67
raji-senthil <strrajeshwari@gmail.com>
2025-09-06 20:48:01 +0530

    Added hello-jenkins maven project with pom.xml

---
+++ src/test/java/com/demo/DemoTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DemoTest {
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
